a2c6d9f882ac7101b8a59de85c2cd961dc2f0872
serhii.mykhailov <serhii.mykhailov@teaminternational.com>
2023-04-05 18:06:51 +0300

    no message

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,4 +4,5 @@ Console.WriteLine("Small conversation with World!");
 Console.WriteLine("Small conversation with World! (2)");
 Console.WriteLine("Small conversation with World! (3)");
 Console.WriteLine("Small conversation with World! (4)");
+Console.WriteLine("Small conversation with World! (5)");
 Console.ReadKey();
